23784e6c219bc88b67fad891a16b38d55c0e6931
TechWithKen <ti664469@gmail.com>
2025-12-08 15:28:34 +0100

    Cleaned up the code to ensure that all the hardcoded portion of the code is removed to avoid error when anybody picks it up. Ensuring that the codebase is usable by anybody anywhere in the world.

--- convert_images_to_PDF.py
+++ convert_images_to_PDF.py
@@ -1,14 +1,15 @@
 import img2pdf
 import os
 
-folder = "C:/Users/ti664/OneDrive/Desktop/images"
+folder = "" # Input the folder that contains all the images you need to add to a particular pdf.
 
 # Full paths of images
 images = [os.path.join(folder, f) for f in os.listdir(folder)]
 images.sort()  # ensures proper order
 
+preferred_name_of_pdf = " " # Enter your preferred name for the PDF
 # Convert to PDF
-with open("NewConvertedFiles.pdf", "wb") as f:
+with open(f"{preferred_name_of_pdf}.pdf", "wb") as f:
     f.write(img2pdf.convert(images))
 
 print("PDF created successfully!")
